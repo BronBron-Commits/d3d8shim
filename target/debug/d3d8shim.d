fb8ec48aa334c6c6464dd7d878d5b32295d4d869
BronBron-Commits <brmanley@Outlook.com>
2025-12-04 21:12:39 -0500

    Add .gitignore for target artifacts

--- target/debug/d3d8shim.d
+++
@@ -1,1 +0,0 @@
-/home/unhidra/d3d8shim/target/debug/d3d8shim: /home/unhidra/d3d8shim/src/main.rs
--- target/debug/deps/d3d8shim-1d5943462390856e.d
+++
@@ -1,5 +0,0 @@
-/home/unhidra/d3d8shim/target/debug/deps/d3d8shim-1d5943462390856e.d: src/main.rs
-
-/home/unhidra/d3d8shim/target/debug/deps/d3d8shim-1d5943462390856e: src/main.rs
-
-src/main.rs:
